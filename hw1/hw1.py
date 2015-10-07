eb03ef7beb7ba89693736f68b2b51df521e8bdf3
Sanjana Galgalikar <sgalgalikar@gmail.com>
2015-10-07 00:22:13 -0700

    commiting hw1.py

---
+++ hw1/hw1.py
@@ -0,0 +1,26 @@
+lst = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+s = "This is an HCDE310 test string"
+
+#Exercise 1 (working with a list):
+#a.	Print the first element of lst
+print lst[0]
+
+#b.	Print the fourth element of lst
+print lst[3]
+#c.	Print the first five elements of lst
+print lst[0:5]
+#d.	Print the last element of lst
+print lst[-1]
+#e.	Print the number of items in lst
+print len(lst)
+#Exercise 2 (working with a string):
+#a.	Print the first four characters of s
+print s[0:5]
+#b.	Print the substring "test" from s
+print s[19:23]
+#c.	Print the contents of s starting from the 9th character
+print s[8:]
+#d.	Print the last three characters of s
+print s[27:]
+#e.	Print the number of characters in s
+print len(s)
